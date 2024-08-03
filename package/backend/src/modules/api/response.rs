84aab00006c43d947bd627f467d077a1a944d2d3
suki-9 <suki.music2004@gmail.com>
2024-08-04 05:05:12 +0900

    Add(backend): Add response util to Auth module

---
+++ package/backend/src/modules/api/response.rs
@@ -0,0 +1,27 @@
+use axum::{
+  http::StatusCode,
+  response::{IntoResponse, Response},
+};
+
+pub type ApiResult<T, E = ApiError> = anyhow::Result<T, E>;
+
+pub struct ApiError(anyhow::Error);
+
+impl IntoResponse for ApiError {
+  fn into_response(self) -> Response {
+    (
+      StatusCode::INTERNAL_SERVER_ERROR,
+      format!("Something went wrong: {}", self.0),
+    )
+      .into_response()
+  }
+}
+
+impl<E> From<E> for ApiError
+where
+  E: Into<anyhow::Error>,
+{
+  fn from(err: E) -> Self {
+    Self(err.into())
+  }
+}
